8106d5dd55ebe658451e2451527b0e8b2638e470
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2021-11-05 17:47:52 +0900

    Update urls.py

--- django/iot/airfilter/urls.py
+++ django/iot/airfilter/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url,include
 
 urlpatterns=[
     path('machines_list/',views.machines_list),
-    path('machine/<int:pk>/',views.address),
-    path('login/',views.login),
+    path('machine/<int:id>/',views.machine),
+    path('latest_sensor/<int:id>/',views.latest_sensor),
+    path('latest_seven_days/<int:id>/',views.latest_seven_days),
+    path('latest_thirty_days/<int:id>/',views.latest_thirty_days),
+    #path('login/',views.login),
     url(r'^api-auth/',include('rest_framework.urls',namespace='rest_framework'))
     ]
