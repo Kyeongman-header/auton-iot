b1a071a1836f75d31d329e5e43283ad27442760b
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2021-11-05 15:38:59 +0900

    Update restart_server.sh
    엥 왜 오류뜨지 다시해봄

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-if [ -e /etc/systemd/system/gunicorn.service ] then
-  sudo service gunicorn restart
+if [ -e /etc/systemd/system/gunicorn.service ] 
+then
+sudo service gunicorn restart
 fi
 
-if [ -e /usr/lib/systemd/system/nginx.service ] then
-  sudo service nginx restart
+if [ -e /usr/lib/systemd/system/nginx.service ] 
+then
+sudo service nginx restart
 fi
 
-if [ -e /usr/lib/systemd/system/mosquitto.service ] then
-  sudo service mosquitto restart
+if [ -e /usr/lib/systemd/system/mosquitto.service ]
+then
+sudo service mosquitto restart
 fi
 
-if [ -e /usr/lib/systemd/system/mqtt_postgres.service ] then
-  sudo service mqtt_postgres restart
+if [ -e /usr/lib/systemd/system/mqtt_postgres.service ]
+then
+sudo service mqtt_postgres restart
 fi
