e9f080373e477eae92495745c39e01aa76fac240
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2021-11-05 14:53:06 +0900

    Update restart_server.sh
    fi를 추가해야 되네

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -2,12 +2,16 @@
 
 if [ -e /etc/systemd/system/gunicorn.service ] then
   sudo service gunicorn restart
-  
+fi
+
 if [ -e /usr/lib/systemd/system/nginx.service ] then
   sudo service nginx restart
+fi
 
 if [ -e /usr/lib/systemd/system/mosquitto.service ] then
   sudo service mosquitto restart
+fi
 
 if [ -e /usr/lib/systemd/system/mqtt_postgres.service ] then
   sudo service mqtt_postgres restart
+fi
