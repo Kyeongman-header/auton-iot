54bdc8a94be43c5eada0fee4e171edda5bbe59c8
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2021-11-05 12:39:43 +0900

    Update admin.py
    admin list display들 좀 보기 좋게 수정함

--- django/iot/airfilter/admin.py
+++ django/iot/airfilter/admin.py
@@ -3,18 +3,38 @@ from .models import *
 
 # Register your models here.
 
-class SensorInline(admin.TabularInline):
-    model=Machine
-    extra=3
+# class SensorInline(admin.TabularInline):
+#     model=Machine
+#     extra=3
 
 class MachineAdmin(admin.ModelAdmin):
     readonly_fields=('id',)
     list_display=('id','car_number','pub_date')
-    list_filter=['pub_date']
-    search_fields=['id','car_number']
+    list_filter=['id','car_number','pub_date']
+    search_fields=['id','car_number','pub_date']
+    
+class SensorAdmin(admin.ModelAdmin):
+    list_display=('pub_date','sensor','machine')
+    list_filter=('machine','pub_date')
+    search_fields=['machine','pub_date','sensor']
+
+class AirKoreaAdmin(admin.ModelAdmin):
+    list_display=('pub_date','airkorea','machine')
+    list_filter=('machine','pub_date')
+    search_fields=['machine','pub_date','sensor']
+    
+class SevenDaysAdmin(admin.ModelAdmin):
+    list_display=('seven_days_sensor','seven_days_airkorea','machine')
+    list_filter=('machine')
+    search_fields=['machine']
+    
+class ThirtyDaysAdmin(admin.ModelAdmin):
+    list_display=('thirty_days_sensor','thirty_days_airkorea','machine')
+    list_filter=('machine')
+    search_fields=['machine']
 
 admin.site.register(Machine,MachineAdmin)
-admin.site.register(Sensor)
-admin.site.register(AirKorea)
-admin.site.register(Seven_Days)
-admin.site.register(Thirty_Days)
+admin.site.register(Sensor,SensorAdmin)
+admin.site.register(AirKorea,AirKoreaAdmin)
+admin.site.register(Seven_Days,SevenDayAdmin)
+admin.site.register(Thirty_Days,ThrityDayAdmin)
