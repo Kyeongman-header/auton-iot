05d8bd3ecc817b12d76c715e941cb0e7b12052a9
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2021-11-12 15:39:48 +0900

    Update restart_server.sh
    app 들이 늘어남에 따라 collect static 기능 첨가.

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-if [ -e /etc/systemd/system/gunicorn.service ] 
-then
-sudo service gunicorn restart
-fi
 
-if [ -e /usr/lib/systemd/system/nginx.service ] 
-then
-sudo service nginx restart
-fi
 
 if [ -e /usr/lib/systemd/system/mosquitto.service ]
 then
@@ -18,6 +10,17 @@ fi
 if [ -e /usr/lib/systemd/system/mqtt_postgres.service ]
 then
 sudo service mqtt_postgres restart
+fi
+
+if [ -e /usr/lib/systemd/system/nginx.service ] 
+then
+sudo /usr/bin/python3 /home/ubuntu/django/iot/manage.py collectstatic
+sudo service nginx restart
+fi
+
+if [ -e /etc/systemd/system/gunicorn.service ] 
+then
+sudo service gunicorn restart
 fi
 
 if [ -d /usr/local/lib/python3.8/dist-packages/django_crontab ]
