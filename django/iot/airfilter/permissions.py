e57ee1e6fd74cfe3aac71f991f6207c47fa2bc7a
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2021-11-16 12:26:38 +0900

    Update permissions.py

--- django/iot/airfilter/permissions.py
+++ django/iot/airfilter/permissions.py
@@ -2,9 +2,10 @@ from rest_framework import permissions
 
 class AdminWriteOrUserReadOnly(permissions.BasePermission):
     def has_permission(self,request,view):
-        
         if request.user.is_authenticated:
             if request.user.is_staff :
+                return True
+            elif request.user.has_perm('airfilter.api_write'):
                 return True
             elif request.method in permissions.SAFE_METHODS :
                 return True
