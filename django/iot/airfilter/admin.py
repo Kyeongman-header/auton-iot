7194ce617351fc98ff17c8c306d8fca33cb67924
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2021-11-19 11:48:11 +0900

    Update admin.py

--- django/iot/airfilter/admin.py
+++ django/iot/airfilter/admin.py
@@ -33,11 +33,18 @@ class UserAdmin(BaseUserAdmin):
     ordering=('username',)
     filter_horizontal=()
 
-class MachineAdmin(OSMGeoAdmin):
-    list_display=('id','car_number','pub_date','gps')
+class MachineAdmin(admin.ModelAdmin):
+    list_display=('id','car_number','pub_date')
     list_filter=['id','car_number','pub_date']
     search_fields=['id','car_number','pub_date']
-    
+class GPSAdmin(OSMGeoAdmin):
+    list_display=('pub_date','gps','machine')
+    list_filter=('machine','pub_date')
+    search_fields=['machine','pub_date','gps']
+class QRAdmin(admin.ModelAdmin):
+    list_display=('pub_date','qr','machine')
+    list_filter=('machine','pub_date')
+    search_fields=['machine','pub_date','qr']
 class SensorAdmin(admin.ModelAdmin):
     list_display=('pub_date','sensor','machine')
     list_filter=('machine','pub_date')
@@ -57,7 +64,9 @@ class ThirtyDaysAdmin(admin.ModelAdmin):
     list_display=('pub_date','thirty_days_sensor_avg','thirty_days_sensor_max','thirty_days_airkorea_avg','thirty_days_airkorea_max','machine')
     list_filter=['machine']
     search_fields=['machine']
-
+    
+admin.site.register(GPS,GPSAdmin)    
+admin.site.register(QR,QRAdmin)
 admin.site.register(MyUser,UserAdmin)
 admin.site.unregister(Group)
 admin.site.register(Machine,MachineAdmin)
