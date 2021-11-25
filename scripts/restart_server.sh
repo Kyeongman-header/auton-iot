829f48a2c04d29d180f230b8e5ec05d780ddf3bd
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2021-11-25 11:41:43 +0900

    Update restart_server.sh

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -2,15 +2,7 @@
 
 
 
-if [ -e /usr/lib/systemd/system/mosquitto.service ]
-then
-sudo service mosquitto restart
-fi
 
-if [ -e /usr/lib/systemd/system/mqtt_postgres.service ]
-then
-sudo service mqtt_postgres restart
-fi
 
 if [ -e /usr/lib/systemd/system/nginx.service ] 
 then
