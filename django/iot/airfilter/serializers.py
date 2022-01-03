6750089a44deaffa1cdaa20c2ac653548506f4a7
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2022-01-03 11:06:31 +0900

    Update serializers.py

--- django/iot/airfilter/serializers.py
+++ django/iot/airfilter/serializers.py
@@ -1,7 +1,21 @@
 from .models import *
 from rest_framework import serializers
 
+class HoursSensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Hours_sensor
+        fields=['machine','hours','pub_date']
 
+class DaysSensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Days_sensor
+        fields=['machine','days','pub_date']
+
+class WeeksSensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Weeks_sensor
+        fields=['machine','weeks','pub_date']
+        
 class GPSSerializer(serializers.ModelSerializer):
     class Meta:
         model=GPS
@@ -29,13 +43,13 @@ class AirKoreaSerializer(serializers.ModelSerializer):
         model=AirKorea
         fields=['machine','airkorea','pub_date']
         
-class SevenDaysSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Seven_Days
-        fields=['machine','seven_days_sensor_avg','seven_days_sensor_max','seven_days_airkorea_avg','seven_days_airkorea_max','pub_date']
+# class SevenDaysSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model=Seven_Days
+#         fields=['machine','seven_days_sensor_avg','seven_days_sensor_max','seven_days_airkorea_avg','seven_days_airkorea_max','pub_date']
         
-class ThirtyDaysSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Thirty_Days
-        fields=['machine','thirty_days_sensor_avg','thirty_days_sensor_max','thirty_days_airkorea_avg','thirty_days_airkorea_max','pub_date']
+# class ThirtyDaysSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model=Thirty_Days
+#         fields=['machine','thirty_days_sensor_avg','thirty_days_sensor_max','thirty_days_airkorea_avg','thirty_days_airkorea_max','pub_date']
         
