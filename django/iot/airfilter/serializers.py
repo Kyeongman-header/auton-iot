a184307da9f20d60058bbdc6c07339081a31ca15
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2022-01-03 14:56:31 +0900

    Update serializers.py

--- django/iot/airfilter/serializers.py
+++ django/iot/airfilter/serializers.py
@@ -16,17 +16,17 @@ class WeeksSensorSerializer(serializers.ModelSerializer):
         model=Weeks_sensor
         fields=['machine','weeks','pub_date']
         
-class HoursSensorSerializer(serializers.ModelSerializer):
+class HoursAirKoreaSerializer(serializers.ModelSerializer):
     class Meta:
         model=Hours_airkorea
         fields=['machine','hours','pub_date']
 
-class DaysSensorSerializer(serializers.ModelSerializer):
+class DaysAirKoreaSerializer(serializers.ModelSerializer):
     class Meta:
         model=Days_airkorea
         fields=['machine','days','pub_date']
 
-class WeeksSensorSerializer(serializers.ModelSerializer):
+class WeeksAirKoreaSerializer(serializers.ModelSerializer):
     class Meta:
         model=Weeks_airkorea
         fields=['machine','weeks','pub_date'] 
