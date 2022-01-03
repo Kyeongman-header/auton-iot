8bc4d80941ccc61bc5782ed4b63150dd974f9957
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2022-01-03 14:26:46 +0900

    Update models.py

--- django/iot/airfilter/models.py
+++ django/iot/airfilter/models.py
@@ -96,18 +96,18 @@ class Hours_sensor(models.Model):
     hours=models.FloatField(null=True, blank=True)
     number=models.IntegerField(default=1)
     def __str__(self):
-        return str(self.sensor)
+        return str(self.hours)
 class Days_sensor(models.Model):
     machine=models.ForeignKey(Machine,on_delete=models.CASCADE)
     pub_date=models.DateTimeField('days date published',default=timezone.localtime)
     days=models.FloatField(null=True, blank=True)
     number=models.IntegerField(default=1)
     def __str__(self):
-        return str(self.sensor)
+        return str(self.days)
 class Weeks_sensor(models.Model):
     machine=models.ForeignKey(Machine,on_delete=models.CASCADE)
     pub_date=models.DateTimeField('weeks date published',default=timezone.localtime)
     weeks=models.FloatField(null=True, blank=True)
     number=models.IntegerField(default=1)
     def __str__(self):
-        return str(self.sensor)
+        return str(self.weeks)
