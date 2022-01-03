97f4b2cefda46b7b030880ffab7b2dd1b365663c
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2022-01-03 15:51:30 +0900

    Update views.py

--- django/iot/airfilter/views.py
+++ django/iot/airfilter/views.py
@@ -329,9 +329,9 @@ class SensorViewset(ReadOnlyModelViewSet):
                 m=request.user.machine
             except :
                 return HttpResponse("No machine registered in that user.", status=405)
-            sensors=m.sensor_set.last() # 실시간에서만 쓸 거니깐 가장 마지막 데이터만.
-            sensor_jsons=SensorSerializer(sensors).data
-            return JsonResponse(sensor_jsons,status=200,safe=False)
+            #sensors=m.sensor_set.last() # 실시간에서만 쓸 거니깐 가장 마지막 데이터만.
+            #sensor_jsons=SensorSerializer(sensors).data
+            return JsonResponse(m.sensor_set.last(),status=200,safe=False)
         
     def retrieve(self, request,pk=None):
         if request.user.is_staff :
@@ -357,10 +357,9 @@ class AirKoreaViewset(ReadOnlyModelViewSet):
                 m=request.user.machine
             except :
                 return HttpResponse("No machine registered in that user.", status=405)
-            airkoreas=m.airkorea_set.last()
-            
-            airkorea_jsons=AirKoreaSerializer(airkoreas).data
-            return JsonResponse(airkorea_jsons,status=200,safe=False)
+            #airkoreas=m.airkorea_set.last()
+            #airkorea_jsons=AirKoreaSerializer(airkoreas).data
+            return JsonResponse(m.airkorea_set.last(),status=200,safe=False)
         
     def retrieve(self, request,pk=None):
         if request.user.is_staff :
