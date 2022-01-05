3825f32bc953d6fc5fc12b8073c21a581fe230c8
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2022-01-05 14:33:00 +0900

    Update views.py

--- django/iot/airfilter/views.py
+++ django/iot/airfilter/views.py
@@ -46,7 +46,7 @@ class OnlyMQTTSensorAdd(CreateAPIView,):
                     
                     h.hours=((h.hours*h.number) + air_data.airkorea['khai']) / (h.number+1) 
                     h.number=h.number+1
-                    if h.hours_worst < air_data.airkorea['khai'] :
+                    if h.hours_worst < air_data.airkorea['khai'] or h.hours_worst is None:
                         h.hours_worst=air_data.airkorea['khai']
                     h.save()
                 else :
@@ -62,7 +62,7 @@ class OnlyMQTTSensorAdd(CreateAPIView,):
                     d=m.days_airkorea_set.last()
                     d.days=((d.days*d.number) + air_data.airkorea['khai']) / (d.number+1) 
                     d.number=d.number+1
-                    if d.days_worst < air_data.airkorea['khai'] :
+                    if d.days_worst < air_data.airkorea['khai'] or d.days_worst is None:
                         d.days_worst=air_data.airkorea['khai']
                     d.save()
                 else :
@@ -75,7 +75,7 @@ class OnlyMQTTSensorAdd(CreateAPIView,):
                     w=m.weeks_airkorea_set.last()
                     w.weeks=((w.weeks*w.number) + air_data.airkorea['khai']) / (w.number+1) 
                     w.number=w.number+1
-                    if w.weeks_worst < air_data.airkorea['khai'] :
+                    if w.weeks_worst < air_data.airkorea['khai'] or w.weeks_worst is None:
                         w.weeks_worst=air_data.airkorea['khai']
                     w.save()
                 else :
@@ -93,7 +93,7 @@ class OnlyMQTTSensorAdd(CreateAPIView,):
                     
                     h.hours=((h.hours*h.number) + data['sensor']['P.M 2.5']) / (h.number+1) 
                     h.number=h.number+1
-                    if h.hours_worst < data['sensor']['P.M 2.5'] :
+                    if h.hours_worst < data['sensor']['P.M 2.5'] or h.hours_worst is None:
                         h.hours_worst=data['sensor']['P.M 2.5']
                     h.save()
                 else :
@@ -108,7 +108,7 @@ class OnlyMQTTSensorAdd(CreateAPIView,):
                     d=m.days_sensor_set.last()
                     d.days=((d.days*d.number) + data['sensor']['P.M 2.5']) / (d.number+1) 
                     d.number=d.number+1
-                    if d.days_worst < data['sensor']['P.M 2.5'] :
+                    if d.days_worst < data['sensor']['P.M 2.5'] or d.days_worst is None:
                         d.days_worst=data['sensor']['P.M 2.5']
                     d.save()
                 else :
@@ -121,7 +121,7 @@ class OnlyMQTTSensorAdd(CreateAPIView,):
                     w=m.weeks_sensor_set.last()
                     w.weeks=((w.weeks*w.number) + data['sensor']['P.M 2.5']) / (w.number+1) 
                     w.number=w.number+1
-                    if w.weeks_worst < data['sensor']['P.M 2.5'] :
+                    if w.weeks_worst < data['sensor']['P.M 2.5'] or w.weeks_worst is None:
                         w.weeks_worst=data['sensor']['P.M 2.5']
                     w.save()
                 else :
@@ -260,12 +260,14 @@ class GPSViewset(ModelViewSet):
                     
                     h.hours=((h.hours*h.number) + air_data.airkorea['khai']) / (h.number+1) 
                     h.number=h.number+1
+                    if h.hours_worst < air_data.airkorea['khai'] or h.hours_worst is None:
+                        h.hours_worst=air_data.airkorea['khai']
                     h.save()
                 else :
-                    m.hours_airkorea_set.create(hours=air_data.airkorea['khai'], number=1)
+                    m.hours_airkorea_set.create(hours=air_data.airkorea['khai'],hours_worst=air_data.airkorea['khai'], number=1)
                     
             else :
-                m.hours_airkorea_set.create(hours=air_data.airkorea['khai'], number=1)
+                m.hours_airkorea_set.create(hours=air_data.airkorea['khai'],hours_worst=air_data.airkorea['khai'], number=1)
 
                 
            
@@ -274,22 +276,26 @@ class GPSViewset(ModelViewSet):
                     d=m.days_airkorea_set.last()
                     d.days=((d.days*d.number) + air_data.airkorea['khai']) / (d.number+1) 
                     d.number=d.number+1
+                    if d.days_worst < air_data.airkorea['khai'] or d.days_worst is None:
+                        d.days_worst=air_data.airkorea['khai']
                     d.save()
                 else :
-                    m.days_airkorea_set.create(days=air_data.airkorea['khai'], number=1)
+                    m.days_airkorea_set.create(days=air_data.airkorea['khai'],days_worst=air_data.airkorea['khai'], number=1)
             else :
-                m.days_airkorea_set.create(days=air_data.airkorea['khai'], number=1)
+                m.days_airkorea_set.create(days=air_data.airkorea['khai'],days_worst=air_data.airkorea['khai'], number=1)
                 
             if m.weeks_airkorea_set.exists() :    
                 if  (datetime.datetime.now(timezone.utc) - m.weeks_airkorea_set.last().pub_date).days/7 < 1:
                     w=m.weeks_airkorea_set.last()
                     w.weeks=((w.weeks*w.number) + air_data.airkorea['khai']) / (w.number+1) 
                     w.number=w.number+1
+                    if w.weeks_worst < air_data.airkorea['khai'] or w.weeks_worst is None:
+                        w.weeks_worst=air_data.airkorea['khai']
                     w.save()
                 else :
-                    m.weeks_airkorea_set.create(weeks=air_data.airkorea['khai'], number=1)
+                    m.weeks_airkorea_set.create(weeks=air_data.airkorea['khai'],weeks_worst=air_data.airkorea['khai'], number=1)
             else :
-                m.weeks_airkorea_set.create(weeks=air_data.airkorea['khai'], number=1)
+                m.weeks_airkorea_set.create(weeks=air_data.airkorea['khai'],weeks_worst=air_data.airkorea['khai'],  number=1)
                 
         
         return JsonResponse(serializer.data,status=201)
